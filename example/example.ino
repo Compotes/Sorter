76ffc1c0e16b393095cb9ee7167ff6a6c924d29d
Gregor Karetka <gregorkaretka999@gmail.com>
2015-02-14 19:26:53 +0100

    - Formating example - Adding example
    Signed-off-by: Gregor Karetka <gregorkaretka999@gmail.com>

--- example/example.ino
+++ example/example.ino
@@ -2,4 +2,28 @@
 
 Sorter s;
 
+void setup() {
+    Serial.begin(9600);
+}
+
+int c = 0;
+void loop() {
+    if (c == 0) {
+        int a[12][2];
+        for (int i = 0; i < 12; i++) {
+            a[i][0] = random(100);
+            a[i][1] = i;
+        }
+  
+        s.sort(a);
+        for (int i = 0; i < 12; i++) {
+            Serial.print(a[i][1]);
+            Serial.print(' ');
+            Serial.println(a[i][0]);
+        }
+        c += 1;
+    }
+}
+
+
 
