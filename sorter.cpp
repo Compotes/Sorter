3125ef255b397b516dbf9a33c87f049047210483
Gregor Karetka <gregorkaretka999@gmail.com>
2015-02-15 15:06:39 +0100

    sorter: Add function to find minimums.
    - Refactoring
    - Formating
    
    Signed-off-by: Gregor Karetka <gregorkaretka999@gmail.com>

--- sorter.cpp
+++ sorter.cpp
@@ -7,7 +7,7 @@ namespace sorter {
         *b = c;
     }
 
-    void sort(int s[12][2]) {
+    void sort_max(int s[12][2]) {
         int max = s[0][0], max2 = s[1][0];
         int max_pos = s[0][1], max2_pos = s[1][1];
 
@@ -33,4 +33,32 @@ namespace sorter {
         swap(&s[0][1], &s[max_pos][1]);
         swap(&s[1][1], &s[max2_pos][1]);
     }
+
+    void sort_min(int s[12][2]) {
+        int min = s[0][0], min2 = s[1][0];
+        int min_pos = s[0][1], min2_pos = s[1][1];
+
+        if (min2 < min) {
+            swap(&min, &min2);
+            swap(&min_pos, &min2_pos);
+        }
+
+        for (int j = 2; j < 12; j++) {
+            if (s[j][0] < min) {
+                min2 = min;
+                min2_pos = min_pos;
+                min = s[j][0];
+                min_pos = s[j][1];
+            } else if (s[j][0] < min2) {
+                min2 = s[j][0];
+                min2_pos = s[j][1];
+            }
+        }
+
+        swap(&s[0][0], &s[min_pos][0]);
+        swap(&s[1][0], &s[min2_pos][0]);
+        swap(&s[0][1], &s[min_pos][1]);
+        swap(&s[1][1], &s[min2_pos][1]);
+    }
 }
+
